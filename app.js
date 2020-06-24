28ad35a23ca5026e41d9366d628b1435cb738b99
Ezequiel Lopez <56897904+ezeloop@users.noreply.github.com>
2020-06-24 01:03:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('ejs');
 const app = express();
 const router = require('./router');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
